186e2d269963e4cac9cc1a4c2120aa39ccb0cc76
박소은 <skyhills13@gmail.com>
2017-02-06 18:28:06 +0900

    soeun 4장 임시

--- exercises/src/main/scala/fpinscala/errorhandling/soeun/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/soeun/Option.scala
@@ -1,7 +1,6 @@
 package fpinscala.errorhandling.soeun
 
 //import scala.{Option => _, Some => _, Either => _, _} // hide std library `Option`, `Some` and `Either`, since we are writing our own in this chapter
-
 /*4.1*/
 //map과 getOrElse를 제외한 모든 함수는 패턴 매칭 없이 구현할 수 있어야
 sealed trait Option[+A] {
@@ -10,41 +9,45 @@ sealed trait Option[+A] {
     case Some(a) => Some(f(a))
   }
 
-  def getOrElse[B>:A](default: => B): B = this match {
+  def getOrElse[B >: A](default: => B): B = this match {
     case None => default
     case Some(a) => a
   }
+
   //패턴 매칭을 이용하지 않고 하는 방법을 모르겠음
   def flatMap[B](f: A => Option[B]): Option[B] = this match {
     case None => None
     case Some(a) => f(a)
   }
 
-//  def orElse[B>:A](ob: => Option[B]): Option[B] = this match {
-//    case None => ob
-//    case
-//  }
+  def orElse[B>:A](ob: => Option[B]): Option[B] = this match {
+    case None => ob
+    case _ => this
+  }
 
-//  def filter(f: A => Boolean): Option[A] = this match {
-    //왜 안될까
-//    case None => None
-//    case Some(a) =>
-//      if f(a) this
-//      else None
-//  }
+  def filter1(f: A => Boolean): Option[A] = this match {
+    case None => None
+    case Some(a) =>
+      if (f(a)) this
+      else None
+  }
+
   def filter(f: A => Boolean): Option[A] = {
     flatMap(a =>
-      if(f(a)) Some(a)
+      if (f(a)) Some(a)
       else None
     )
   }
+
   def main(args: Array[String]): Unit = {
-    val x = List(1,2,3)
-//    Some(List(1,2,3)).map(x => x + 1)
-//    println(Some(x).map(a => a + 1))
+    val x = 1
+    println(Some(x).map(x => x + 1))
+    println(Some(x).map(a => a + 1))
   }
 }
+
 case class Some[+A](get: A) extends Option[A]
+
 case object None extends Option[Nothing]
 
 object Option {
@@ -77,27 +80,49 @@ object Option {
 //  }
   /*4.3*/
   //74p참고
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = {
-    a flatMap(aa =>
-      b map(bb =>
-        f(aa,bb)
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = {
+//    a.map(aa => {b.map(bb => f(aa,bb))})
+//    a.map(aa => {b.map(aa+bb)})
+//    a.map(aa => {Some(aa+bb)})
+//    Some(Some(aa+bb)))
+//
+//    a.map(aa => b.map(bb =>f(aa, bb)))
+//    a.map(aa => Some(f(aa, bb)))
+//    Some(Some(f(aa, bb))))
+//
+//    a.flatMap(aa => Option(f(aa, bb)))
+//    Option(f(aa, bb))
+//
+//    a.flatMap(a => Some(a+2))
+//    Some(3)
+    a flatMap (aa =>
+      b map (bb =>
+        f(aa, bb)
         )
       )
   }
+
   /*4.4*/
-//  def sequence[A](a: List[Option[A]]): Option[List[A]] = a match {
-//    case Nil => Some(Nil)
-//    case h::t =>
-//  }
+  //  def sequence[A](a: List[Option[A]]): Option[List[A]] = a match {
+  //    case Nil => Some(Nil)
+  //    case h::t =>
+  //  }
   /*4.5*/
-  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = a match {
-    case Nil => Some(Nil)
-    case h::t =>
-  }
+  //  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = a match {
+  //    case Nil => Some(Nil)
+  //    case h::t =>
+  //  }
 
   //traverse이용
-//  def sequence[A](a: List[Option[A]]): Option[List[A]] = a match {
-//    case Nil => Some(Nil)
-//    case h::t =>
-//  }
+  //  def sequence[A](a: List[Option[A]]): Option[List[A]] = a match {
+  //    case Nil => Some(Nil)
+  //    case h::t =>
+  //  }
+}
+
+object Test {
+  def main(args: Array[String]): Unit = {
+    val f = (a: Int, b: Int) => a + b
+    println(Option.map2(Some(1), Some(2))(f));
+  }
 }
